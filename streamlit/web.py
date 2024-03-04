4f8c59f41c8b34d8c01adf5d607347384a047c77
pudcharapon2302 <pudcharapon.yi.66@ubu.ac.th>
2024-03-05 02:59:54 +0700

    5/3/2567

--- streamlit/web.py
+++ streamlit/web.py
@@ -3,7 +3,7 @@ import time
 
 page_bg_img = '''<style>
 .stApp {
-    background-image: url("https://images.unsplash.com/photo-1484482340112-e1e2682b4856?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+    background-image: url("https://images.unsplash.com/photo-1546519638-68e109498ffc?q=80&w=2090&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
     background-size: cover;
 }
 </style>'''
